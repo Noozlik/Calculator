be1d2bdaf5866c1742d5e6dba2cd6282f7c558a5
Noozlik <mr.fialochka@mail.ru>
2023-12-05 13:09:52 +0300

    history correction, adaptive display

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let display= document.querySelector(".sum");
 let history = document.querySelector(".calc-history");
-/*let buttons = Array.from(document.querySelectorAll(".btn"));*/
 let but = document.querySelectorAll(".btn");
-// const arrayNumbers = ['0','1','2','3','4','6,','7','8','9'];
-// const arraySign = ['+','-','*','/','%'];
 let stack = [];
 
-// function push(item) {
-//     stack.push(item);
-// }
-
-// function look() {
-//     return stack[stack.length - 1];
-// }
+function sizeSymbol (element) {
+    if (element.innerText.length > 7) {
+        element.style.fontSize = "45px";
+    }
+    if (element.innerText.length > 11) {
+        element.innerText = element.innerText.slice(0, 11);
+    }
+}
 
 function plus (a, b) {
     return a + b;
@@ -33,10 +31,6 @@ function division (a, b) {
         return "Error!"
     }
 }
-
-// function plusMinus(a) {
-//     return -1 * a;
-// }
 
 function percent (a, b) {
     return (a * b) / 100;
@@ -71,41 +65,11 @@ function equals() {
             break;
         }
     }
-    // history.innerText = "";
     history.innerText += expression + " = " + result + "\n";
     return result;
 }
 
-// function equals() {
-//     let expression = display.innerText;
-//     let result = null;
-//     for (let i = 0; i < arraySign.length; i++) {
-//         let signIndex = expression.indexOf(arraySign[i]);
-//         if (signIndex !== -1) {
-//             let num1 = parseFloat(expression.slice(0, signIndex));
-//             let num2 = parseFloat(expression.slice(signIndex + 1));
-//             switch (arraySign[i]) {
-//                 case "+":
-//                     result = plus(num1, num2);
-//                     break;
-//                 case "-":
-//                     result = minus(num1, num2);
-//                     break;
-//                 case "*":
-//                     result = multi(num1, num2);
-//                     break;
-//                 case "/":
-//                     result = division(num1, num2);
-//                     break;
-//                 case "%":
-//                     result = percent(num1, num2);
-//                     break;
-//             }
-//             break;
-//         }
-//     }
-//     return result;
-// }
+
 
 but.forEach((item)=> {
     item.addEventListener("click", function (event) {
@@ -115,7 +79,12 @@ but.forEach((item)=> {
             case "x":
             case "/":
             case "%":
-                if (stack.pop() !== "+" && stack.pop() !== "-" && stack.pop() !== "*" && stack.pop() !== "/") {
+                if (
+                    stack[stack.length - 1] !== "+" &&
+                    stack[stack.length - 1] !== "-" &&
+                    stack[stack.length - 1] !== "*" &&
+                    stack[stack.length - 1] !== "/"
+                ) {
                     display.innerText += event.target.innerText;
                 }
                 break;
@@ -126,9 +95,6 @@ but.forEach((item)=> {
                     display.innerText = '-' + display.innerText;
                 }
                 break;
-            /*case "%":
-                display.innerText = percent(num1, num2);
-                break;*/
             case "=":
                 display.innerText = equals();
                 break;
@@ -140,102 +106,10 @@ but.forEach((item)=> {
                 if (display.innerText === "0" && event.target.innerText !== ".") {
                     display.innerText = event.target.innerText;
                 } else {
-                    display.innerText += event.target.innerText;
+                        display.innerText += event.target.innerText;
+                    }
                 }
-        }
         stack.push(event.target.innerText);
+        sizeSymbol(display);
     });
 });
-
-/*function clearAll() {
-    display.innerText = "0";
-}*/
-
-/*function equals (a, b) {
-    switch (operator) {
-        case "+":
-            return plus(a, b);
-        case "-":
-            return minus(a, b);
-        case "*":
-            return multi(a, b);
-        case "/":
-            return division(a, b);
-        case "%":
-            return percent(a, b);
-        case "+/-":
-            return plusMinus(a, b);
-        default:
-            return "Error!";
-    }
-}*/
-
-/*but.forEach((item)=> {
-    item.addEventListener("click", function (event) {
-        switch (event.target.innerText) {
-            case "+":
-                display.innerText = plus(display.innerText);
-                break;
-            case "-":
-                display.innerText = minus();
-                break;
-            case "*":
-                display.innerText = multi();
-                break;
-            case "/":
-                display.innerText = division();
-                break;
-            case "+/-":
-                display.innerText = plusMinus(display.innerText);
-                break;
-            case "%":
-                display.innerText = percent();
-                break;
-            case "AC":
-                display.innerText = "0";
-                break;
-            case "=":
-                display.innerText = equals();
-                break;
-            default:
-                if(display.innerText === "0" && event.target.innerText !== ".") {
-                    display.innerText = event.target.innerText;
-                }
-                else {
-                    display.innerText += event.target.innerText;
-                }
-        }
-    })
-})*/
-
-
-/*buttons.map((btn) => {
-    btn.addEventListener("click", function (event) { //добавляю обработчик событий; (e) елемент на который кликаю
-        console.log(event);
-        switch (event.target.innerText) {
-            case "AC":
-                display.innerText = "0";    // сделать не через eval повесить на каждую кнопку клик
-                break;
-            case "=":
-                display.innerText = eval(display.innerText);  //не нравится что использую innerText
-                break;
-            case "+/-":
-                display.innerText = "-"; // поправить минус что бы не менял себя с числа
-                break;
-            case "%":
-                let meaning = display.innerText + "/100";  // поправить процент одно действие
-                display.innerText = eval(meaning);
-                break;
-            case "x":
-                display.innerText = display.innerText + "*";
-                break;
-            default:
-                if(display.innerText === "0" && event.target.innerText !== ".") {
-                    display.innerText = event.target.innerText;
-                }
-                else {
-                    display.innerText += event.target.innerText;
-                }
-        }
-    });
-});*/
