1037ca9677438b5b3f5c48ce808691f84ccca583
Noozlik <mr.fialochka@mail.ru>
2024-03-27 14:47:51 +0300

    color,adaptive,sound when pressed,general edits

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let display= document.querySelector(".sum");
+let displayResult = document.querySelector(".calc-sum");
 let history = document.querySelector(".calc-history");
+
 let but = document.querySelectorAll(".btn");
-let resetHistoryBtn = document.getElementById("reset-history");
+let resetHistoryBtn = document.querySelector(".reset-history");
+
 let wrapper = document.querySelector(".calculator-wrapper");
-let toggleBtn = document.getElementById("toggleBtn");
+let toggleBtn = document.querySelector(".toggleBtn");
+let historyBoard = document.querySelector(".history-board");
+
 let stack = [];
 
 toggleBtn.addEventListener("click", function () {
@@ -13,59 +18,89 @@ toggleBtn.addEventListener("click", function () {
         wrapper.classList.add("open");
     }
 });
-
-document.addEventListener("keydown", function(event) {
-    let key = event.key; // Получаем нажатую клавишу
-
-    // Проверка является ли нажатая клавиша цифрой или знаком
-    if (!Number.isNaN(Number(key))|| key === "+" || key === "-" || key === "x" || key === "/" || key === "%") {
-        // Обрабатываем ввод цифры или знака
-        if (display.innerText === "0" && key !== ".") {
-            display.innerText = key;
-        } else {
-            display.innerText += key;
-        }
-
-        stack.push(parseFloat(key));
-        // sizeSymbol(display);
-    }
-    if (display.innerText === "0") {
-        display.innerText = event.target.innerText;
-    }
-    // Проверка, была ли нажата клавиша "Enter"
-    if (key === "Enter") {
-        display.innerText = equals();
-    }
-    // Проверка, была ли нажата клавиша "Escape"
-    if (key === "Escape") {
-        display.innerText = "0"; // Обнуление калькулятор
-        stack = [];
-    }
-    // Проверка для удаления последнего символа или цифры
-    if (key === "Backspace") {
-        let currentText = display.innerText;
-        display.innerText = currentText.slice(0, -1);
-    }
-    if (key === "." || key === "ю") {
-        if (!display.innerText.includes(".")) {
-            display.innerText += ".";
-        }
+historyBoard.addEventListener("click", function () {
+    if (wrapper.classList.contains("open-window")) {
+        wrapper.classList.remove("open-window");
+    } else {
+        wrapper.classList.add("open-window");
     }
 });
 
-// Функция для сохранения истории в локальное хранилище
-function saveHistory(expression) {
-    let currentHistory = localStorage.getItem('history');
-    if (currentHistory) {
-        currentHistory = JSON.parse(currentHistory);
-    } else {
-        currentHistory = [];
+document.addEventListener("keydown", function(event) {
+    let key = event.key;
+    switch (key) {
+        case "+":
+            display.innerText += key;
+            stack.push(parseFloat(key));
+            break;
+        case "-":
+            display.innerText += key;
+            stack.push(parseFloat(key));
+            break;
+        case "x":
+            display.innerText += key;
+            stack.push(parseFloat(key));
+            break;
+        case "/":
+            display.innerText += key;
+            stack.push(parseFloat(key));
+            break;
+        case "%":
+            display.innerText += key;
+            stack.push(parseFloat(key));
+            break;
+        case ".":
+            if (!display.innerText.includes(".")) {
+                display.innerText += key;
+            }
+            break;
+        case "Enter":
+            display.innerText = equals();
+            break;
+        case "Escape":
+            display.innerText = "0";
+            stack = [];
+            break;
+        case "Backspace":
+            let currentText = display.innerText;
+            display.innerText = currentText.slice(0, -1);
+            break;
+        default:
+            if (!Number.isNaN(Number(key))) {
+                if (display.innerText === "0" && key !== ".") {
+                    display.innerText = key;
+                } else {
+                    display.innerText += key;
+                }
+
+                stack.push(parseFloat(key));
+            }
+            break;
     }
-    currentHistory.push({ expression });
-    localStorage.setItem('history', JSON.stringify(currentHistory));
+});
+
+resetHistoryBtn.addEventListener("click", function () {
+    history.innerText = "";
+    localStorage.removeItem('history');
+});
+
+function playSound() {
+    const audio = new Audio("sound/click_button.mp3");
+    audio.play();
 }
 
-// Функция для загрузки истории из локального хранилища
+// Function for saving history to local storage
+function saveHistory(expression) {
+    let historyArray = [];
+    let currentHistory = localStorage.getItem('history');
+    if (currentHistory) {
+        historyArray = JSON.parse(currentHistory);
+    }
+    historyArray.push({ expression });
+    localStorage.setItem('history', JSON.stringify(historyArray));
+}
+
+// Function for downloading history from local storage
 function loadHistory() {
     let currentHistory = localStorage.getItem('history');
     if (currentHistory) {
@@ -76,6 +111,7 @@ function loadHistory() {
     }
 }
 
+//Function to reduce font
 function sizeSymbol (element) {
     if (element.innerText.length > 8) {
         element.style.fontSize = "30px";
@@ -85,6 +121,7 @@ function sizeSymbol (element) {
     }
 }
 
+//Functions for the calculator
 function plus (a, b) {
     return a + b;
 }
@@ -105,8 +142,15 @@ function percent(a) {
     return a / 100;
 }
 
-let isEqualSignPressed = false;
+function cube(a) {
+    return a * a * a;
+}
 
+function square(a) {
+    return a * a;
+}
+
+let isEqualSignPressed = false;
 function equals() {
     let result = null;
     let sign = stack[1];
@@ -135,74 +179,88 @@ function equals() {
     if (result) {
         result = parseFloat(result.toFixed(3));
     }
-        history.innerText += num1 + " " + sign + " " + num2 + " = " + result + "\n";
-        display.innerText = result;
-        isEqualSignPressed = true;
 
-        // if (isEqualSignPressed) {
-            stack = [result, sign, num2];
-            isEqualSignPressed = false;
-        // }
-        return result;
+    history.innerText += num1 + " " + sign + " " + num2 + " = " + result + "\n";
+    displayResult.innerText = num1 + " " + sign + " " + num2 + " = " + result;
+    isEqualSignPressed = true;
+    saveHistory();
+    stack = [result, sign, num2];
+    isEqualSignPressed = false;
+    return result;
 }
-but.forEach((item)=> {
-    item.addEventListener("click", function (event) {
-        switch (event.target.innerText) {
-            case "+":
-                stack.push(display.innerText);
-                stack.push(event.target.innerText);
-                display.innerText = "";
-                break;
-            case "-":
-                stack.push(display.innerText);
-                stack.push(event.target.innerText);
-                display.innerText = "";
-                break;
-            case "x":
-                stack.push(display.innerText);
-                stack.push(event.target.innerText);
-                display.innerText = "";
-                break;
-            case "÷":
-                stack.push(display.innerText);
-                stack.push(event.target.innerText);
-                display.innerText = "";
-                break;
-            case "%":
-                display.innerText = percent(display.innerText);
-                history.innerText += display.innerText;
-                break;
-            case "+/-":
-                if (display.innerText[0] === "-") {
-                    display.innerText = display.innerText.slice(1);
-                } else {
-                    display.innerText = '-' + display.innerText;
-                }
-                break;
-            case "=":
-                stack.push(display.innerText);
-                display.innerText = equals();
-                break;
-            case "AC":
-                stack = [];
-                display.innerText = "0";
-                break;
-            default:
-                if (display.innerText === "0" && event.target.innerText !== ".") {
-                    display.innerText = event.target.innerText;
-                } else {
-                    display.innerText += event.target.innerText;
-                }
-        }
-        console.log(stack);
-        console.log(typeof stack[stack.length - 1]);
-        // secondWindow.innerText += event.target.innerText;
-        sizeSymbol(display);
-    });
-});
 
-loadHistory();
-resetHistoryBtn.addEventListener("click", function () {
-    history.innerText = ""; // Очистить историю вычислений
-    localStorage.removeItem('history'); // Удалить данные из localStorage
+function handleButtonClick(event) {
+    switch (event.target.innerText) {
+        case "+":
+            stack.push(display.innerText);
+            stack.push(event.target.innerText);
+            display.innerText = "";
+            break;
+        case "-":
+            stack.push(display.innerText);
+            stack.push(event.target.innerText);
+            display.innerText = "";
+            break;
+        case "x":
+            stack.push(display.innerText);
+            stack.push(event.target.innerText);
+            display.innerText = "";
+            break;
+        case "÷":
+            stack.push(display.innerText);
+            stack.push(event.target.innerText);
+            display.innerText = "";
+            break;
+        case "sin":
+            display.innerText = sin(display.innerText);
+            history.innerText += display.innerText;
+            break;
+        case "%":
+            display.innerText = percent(display.innerText);
+            history.innerText += display.innerText;
+            break;
+        case "+/-":
+            if (display.innerText[0] === "-") {
+                display.innerText = display.innerText.slice(1);
+            } else {
+                display.innerText = '-' + display.innerText;
+            }
+            break;
+        case "x²":
+            display.innerText = square(display.innerText);
+            history.innerText += display.innerText;
+            break;
+        case "x³":
+            display.innerText = cube(display.innerText);
+            history.innerText += display.innerText;
+            break;
+        case "=":
+            stack.push(display.innerText);
+            display.innerText = equals();
+            break;
+        case "AC":
+            stack = [];
+            display.innerText = "0";
+            displayResult.innerText = "0";
+            break;
+        default:
+            if (display.innerText === "0" && event.target.innerText !== ".") {
+                display.innerText = event.target.innerText;
+            } else {
+                display.innerText += event.target.innerText;
+            }
+    }
+}
+
+but.forEach((item) => {
+    item.addEventListener("click", handleButtonClick);
 });
+loadHistory();
+
+const buttons = document.getElementsByClassName("btn");
+const audio = new Audio("sound/click_button.mp3");
+Array.prototype.forEach.call(buttons, function (button) {
+      button.addEventListener("click", function () {
+             audio.play();
+            });
+        });
